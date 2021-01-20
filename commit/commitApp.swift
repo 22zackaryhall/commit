9e8bdcbaab6dc45886f9203d4652d995932b32c9
p.m.student <zmhall@students.tulsatech.edu>
2021-01-20 13:30:49 -0600

    Initial Commit

---
+++ commit/commitApp.swift
@@ -0,0 +1,17 @@
+//
+//  commitApp.swift
+//  commit
+//
+//  Created by P.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct commitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
